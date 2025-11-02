d1127eb7379d9efb306e7c3b61c0ecf72b5d7256
SWH03403 <106224482@student.swin.edu.au>
2025-11-02 14:19:37 +0700

    refactor: generic navigation based on current path

--- src/parts/wraps/header.php
+++ src/parts/wraps/header.php
@@ -1,4 +1,17 @@
+<?php function render_nav() {
+	$cur = Request::path();
+	$routes = [
+		'' => 'Home',
+		'jobs' => 'Listings',
+		'apply' => 'Application',
+		'about' => 'About',
+	];
+	foreach ($routes as $path => $disp) {
+		$is_cur = ($cur == $path)? ' class="current-page"' : '';
+		echo "<a href=\"/$path\"$is_cur>$disp</a>";
+	}
+} ?>
 <header class="flex">
-	<nav>
-	</nav>
+	<nav id="global-navigation" class="fill flex"><?php render_nav() ?></nav>
+	<a href="mailto:info@duoordxe.com.au">Contact Us</a>
 </header>
