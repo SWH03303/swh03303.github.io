f8d98631bcf15c1f207f565240d12f0641108841
SWH03403 <106224482@student.swin.edu.au>
2025-11-11 08:20:23 +0700

    refactor(wraps): wrap header nav in unordered list

--- src/parts/wraps/header.php
+++ src/parts/wraps/header.php
@@ -1,4 +1,5 @@
-<?php function render_nav() {
+<?php
+function render_nav() {
 	$cur = Request::path();
 	$routes = [
 		'' => 'Home',
@@ -8,10 +9,12 @@
 	];
 	foreach ($routes as $path => $disp) {
 		$is_cur = ($cur == $path)? ' class="current-page"' : '';
-		echo "<a href=\"/$path\"$is_cur>$disp</a>";
+		echo "<li><a href=\"/$path\"$is_cur>$disp</a></li>";
 	}
 } ?>
 <header class="flex">
-	<nav id="global-navigation" class="fill flex"><?php render_nav() ?></nav>
+	<nav id="global-navigation" class="fill flex">
+		<ul class="fill flex"><?php render_nav() ?></ul>
+	</nav>
 	<a href="mailto:info@duoordxe.com.au">Contact Us</a>
 </header>
