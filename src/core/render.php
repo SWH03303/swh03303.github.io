0ad7ecd57b3c3d2deb93e9966815d7911a935d38
SWH03403 <106224482@student.swin.edu.au>
2025-11-13 09:39:24 +0700

    refactor(render): make `$data` a local variable

--- src/core/render.php
+++ src/core/render.php
@@ -1,13 +1,13 @@
 <?php
-function render(string $component, ?array $g_data = null) {
-	global $data;
-	if (!is_null($g_data)) { $data = $g_data; }
+function render(string $component, ?array $data = null) {
+	global $_g_data;
+	if (is_null($data)) { $data = $_g_data; }
 	require Dirs::COMPONENTS . "/$component.php";
 }
 
-function render_page(array|callable|string $content, array $g_data = []) {
-	global $data;
-	$data = $g_data;
+function render_page(array|callable|string $content, array $data = []) {
+	global $_g_data;
+	$_g_data = $data;
 
 	render('wraps/top');
 	if (is_string($content)) { echo "$content"; }
--- src/parts/links.php
+++ src/parts/links.php
@@ -1,7 +1,6 @@
 <?php
-$routes = $data ?? [];
 $cur = Request::path();
-foreach ($routes as $url => $disp) {
+foreach ($data as $url => $disp) {
 	$is_cur = (str_starts_with($url, '/') && substr($url, 1) === $cur)?
 		' class="current-page"' : '';
 	echo "<li><a href=\"$url\"$is_cur>$disp</a></li>";
